074c50e5e2adff5b1d9aa4ed6c223aeb793c2efc
Thomas Knickman <thomask@surveymonkey.com>
2018-10-15 10:30:34 -0400

    chore(data): refactor key & emoji data
    - rebase off of latest master

---
+++ src/data/emojis/index.js
@@ -0,0 +1,14 @@
+import en_CA from './en-CA';
+import fr_CA from './fr-CA';
+import fr_FR from './fr-FR';
+import de from './de';
+
+export default {
+    // english
+    ...en_CA,
+    // french
+    ...fr_CA,
+    ...fr_FR,
+    // german
+    ...de
+};
