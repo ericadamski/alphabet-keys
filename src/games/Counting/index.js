1d221f80ce00c23b8be9ba1071bdcf9ac962daa1
Tri Nguyen <tri.nguyen@kepleranalytics.com>
2019-10-01 15:45:34 +1000

    add counting game

---
+++ src/games/Counting/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Letters, Emoji } from '../styles';
+import { EmojiContainer } from './styles';
+
+export class Counting extends React.PureComponent {
+	render() {
+		if (!this.props.emoji || !this.props.character) {
+			return (<Letters>Press a number!</Letters>);
+		}
+	
+		return (
+			<>
+				<EmojiContainer>
+					{
+						Array(2).fill(0).map((_, index) => (
+							<Emoji key={index} role="img" aria-label="emoji">{this.props.emoji}</Emoji>
+						))
+					}
+				</EmojiContainer>
+				<Letters>{this.props.character}</Letters>
+			</>
+		)
+	}
+};
---
+++ src/games/Counting/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const EmojiContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+`;
