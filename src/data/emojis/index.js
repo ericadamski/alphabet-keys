8e5e7430df580b534a01c77a2a229c08acb26212
rijdz <rijdzuan@gmail.com>
2019-10-01 20:14:30 +0700

    setup for new language Bahasa (ID)

--- src/data/emojis/index.js
+++ src/data/emojis/index.js
@@ -8,6 +8,7 @@ import nb_NO from './nb-NO';
 import pl from './pl-PL';
 import pt_BR from './pt-BR';
 import sv from './sv';
+import id from './id';
 import it from './it';
 import el_GR from './el-GR';
 import tl from './tl';
@@ -22,6 +23,8 @@ export default {
   ...fr_FR.data,
   // german
   ...de.data,
+  // indonesian (Bahasa)
+  ...id.data,
   // italian
   ...it.data,
   // swedish
--- src/data/languages.js
+++ src/data/languages.js
@@ -8,11 +8,12 @@ import nb_NO from './emojis/nb-NO';
 import pl from './emojis/pl-PL';
 import pt_BR from './emojis/pt-BR';
 import sv from './emojis/sv';
+import id from './emojis/id';
 import it from './emojis/it';
 import el_GR from './emojis/el-GR';
 import tl from './emojis/tl';
 
-const allLangs = [en_CA, es, fr_CA, fr_FR, de, sv, pt_BR, nb_NO, pl, hi, el_GR, it, tl];
+const allLangs = [en_CA, es, fr_CA, fr_FR, de, sv, pt_BR, nb_NO, pl, hi, el_GR, id, it, tl];
 
 export const SUPPORTED_LANGS = allLangs.map(langData => ({
   label: langData.meta.label,
