ccebd3354dd35c5f5260e800cd65eb1e673bddfe
anhek <a.nhek@icloud.com>
2019-10-07 19:17:48 +0200

    cleaned the file + use of prettier

--- src/data/emojis/pt-BR.js
+++ src/data/emojis/pt-BR.js
@@ -4,26 +4,117 @@ export default {
   },
   data: {
     'pt-BR': {
-      97: [['⚓️', 'âncora'], ['🍍', 'abacaxi'], ['🚑', 'ambulância'], ['🐝', 'abelha'], ['🥜', 'amendoim'], ['🌈', 'Arco-íris']],
-      98: [['🍌', 'banana'], ['👶', 'bebê'], ['🦋', 'borboleta'], ['🥔', 'batata'], ['🥦', 'brócolis'], ['🥖', 'baguete'], ['⛄️', 'Boneco de neve'] ['🥓', 'bacon'], ['🚲', 'bicicleta'], ['🐳', 'Baleia']],
-      99: [['🦐', 'camarão'], ['🍫', 'chocolate'], ['☕', 'café'], ['🥄', 'colher'], ['🏠', 'casa'], ['🚗', 'carro'], ['🥕', 'cenoura'], ['🍄', 'cogumelo'], ['🐍', 'cobra'], ['🦉', 'coruja'], ['🍒', 'Cereja'], ['🐰', 'Coelho'], ['🥥', 'Coco']],
-      100: [['🦕', 'dinossauro'], ['🍩', 'donut'], ['🎲', 'dado'], ['💵', 'dinheiro'], ['🐉', 'dragão'], ['💎', 'Diamante'], ['🦷', 'Dente']],
+      97: [
+        ['⚓️', 'âncora'],
+        ['🍍', 'abacaxi'],
+        ['🚑', 'ambulância'],
+        ['🐝', 'abelha'],
+        ['🥜', 'amendoim'],
+        ['🌈', 'Arco-íris']
+      ],
+      98: [
+        ['🍌', 'banana'],
+        ['👶', 'bebê'],
+        ['🦋', 'borboleta'],
+        ['🥔', 'batata'],
+        ['🥦', 'brócolis'],
+        ['🥖', 'baguete'],
+        ['⛄️', 'Boneco de neve'],
+        ['🥓', 'bacon'],
+        ['🚲', 'bicicleta'],
+        ['🐳', 'Baleia']
+      ],
+      99: [
+        ['🦐', 'camarão'],
+        ['🍫', 'chocolate'],
+        ['☕', 'café'],
+        ['🥄', 'colher'],
+        ['🏠', 'casa'],
+        ['🚗', 'carro'],
+        ['🥕', 'cenoura'],
+        ['🍄', 'cogumelo'],
+        ['🐍', 'cobra'],
+        ['🦉', 'coruja'],
+        ['🍒', 'Cereja'],
+        ['🐰', 'Coelho'],
+        ['🥥', 'Coco']
+      ],
+      100: [
+        ['🦕', 'dinossauro'],
+        ['🍩', 'donut'],
+        ['🎲', 'dado'],
+        ['💵', 'dinheiro'],
+        ['🐉', 'dragão'],
+        ['💎', 'Diamante'],
+        ['🦷', 'Dente']
+      ],
       101: [['🐿', 'esquilo'], ['🐘', 'elefante']],
       102: [['👻', 'fantasma'], ['🐜', 'formiga'], ['🚀', 'foquete']],
-      103: [['🦒', 'girafa'], ['🦍', 'gorila'], ['🐱', 'gato'], ['🐔', 'galinha']],
+      103: [
+        ['🦒', 'girafa'],
+        ['🦍', 'gorila'],
+        ['🐱', 'gato'],
+        ['🐔', 'galinha']
+      ],
       104: [['🚁', 'helicóptero'], ['🐹', 'hamster']],
       105: [['☃️', 'inverno'], ['⛪', 'igreja'], ['🧲', 'Imã']],
       106: [['🐗', 'javali'], ['🐊', 'jacaré'], ['📰', 'jornal']],
       107: [['🥝', 'kiwi']],
-      108: [['🐺', 'lobo'], ['🦁', 'leão'], ['🌕' ,'lua'], ['💡', 'Lâmpada'], ['🦑', 'Lula']],
-      109: [['🐵', 'macaco'], ['🦇', 'morcego'], ['🍉', 'melancia'], ['🍎', 'maçã'], ['🍓', 'morango'], ['🍯', 'Mel'],['🥭', 'Manga']],
+      108: [
+        ['🐺', 'lobo'],
+        ['🦁', 'leão'],
+        ['🌕', 'lua'],
+        ['💡', 'Lâmpada'],
+        ['🦑', 'Lula']
+      ],
+      109: [
+        ['🐵', 'macaco'],
+        ['🦇', 'morcego'],
+        ['🍉', 'melancia'],
+        ['🍎', 'maçã'],
+        ['🍓', 'morango'],
+        ['🍯', 'Mel'],
+        ['🥭', 'Manga']
+      ],
       110: [['👃', 'nariz'], ['🌃', 'noite'], ['☁', 'nuvem']],
       111: [['👂', 'orelha'], ['👁', 'olho'], ['🐑', 'ovelha']],
-      112: [['🐼', 'panda'], ['🐷', 'porco'], ['🐧', 'pinguim'], ['🦆', 'pato'], ['🐟', 'peixe'], ['🤡', 'palhaço'], ['🌶', 'Pimenta'], ['🦚', 'Pavão'], ['🥞', 'Panquecas']],
+      112: [
+        ['🐼', 'panda'],
+        ['🐷', 'porco'],
+        ['🐧', 'pinguim'],
+        ['🦆', 'pato'],
+        ['🐟', 'peixe'],
+        ['🤡', 'palhaço'],
+        ['🌶', 'Pimenta'],
+        ['🦚', 'Pavão'],
+        ['🥞', 'Panquecas']
+      ],
       113: [['🧀', 'queijo'], ['🔲', 'quadrado'], ['🧩', 'Quebra-cabeça']],
-      114: [['🤖', 'robô'], ['🦊', 'raposa'], ['🐭', 'rato'], ['⏰', 'Relógio'], ['🌹', 'Rosa']],
-      115: [['🧜', 'sereia'], ['🍣', 'sushi'], ['🍦', 'sorvete'], ['🐸', 'sapo'], ['🛰️', 'satelite'], ['☀️', 'Sol'], ['🥗', 'Salada']],
-      116: [['🐆', 'tigre'], ['🐢', 'tartaruga'], ['🌮', 'taco'], ['🚆', 'trem'], ['☎️', 'Telefone'], ['☹️', 'Triste'], ['🌍', 'Terra']],
+      114: [
+        ['🤖', 'robô'],
+        ['🦊', 'raposa'],
+        ['🐭', 'rato'],
+        ['⏰', 'Relógio'],
+        ['🌹', 'Rosa']
+      ],
+      115: [
+        ['🧜', 'sereia'],
+        ['🍣', 'sushi'],
+        ['🍦', 'sorvete'],
+        ['🐸', 'sapo'],
+        ['🛰️', 'satelite'],
+        ['☀️', 'Sol'],
+        ['🥗', 'Salada']
+      ],
+      116: [
+        ['🐆', 'tigre'],
+        ['🐢', 'tartaruga'],
+        ['🌮', 'taco'],
+        ['🚆', 'trem'],
+        ['☎️', 'Telefone'],
+        ['☹️', 'Triste'],
+        ['🌍', 'Terra']
+      ],
       117: [['🦄', 'unicórnio'], ['🍇', 'uva']],
       118: [['🐮', 'vaca'], ['🧛‍♂️', 'vampiro'], ['🌋', 'vulcão']],
       119: [],
@@ -31,4 +122,4 @@ export default {
       122: [['🦓', 'zebra'], ['🧟', 'zumbi'], ['🤐', 'Zíper']]
     }
   }
-}
+};
