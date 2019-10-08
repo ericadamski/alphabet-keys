7a7f1d41a651e0a37bb582ff33899451f2cc6f6a
J. Tran <jyntran@gmail.com>
2019-10-08 01:11:54 -0400

    Add Vietnamese

--- src/data/languages.js
+++ src/data/languages.js
@@ -14,6 +14,7 @@ import sv from './emojis/sv';
 import it from './emojis/it';
 import el_GR from './emojis/el-GR';
 import tl from './emojis/tl';
+import vi from './emojis/vi';
 
 const allLangs = [
   da_DK,
@@ -31,7 +32,8 @@ const allLangs = [
   hi,
   el_GR,
   it,
-  tl
+  tl,
+  vi
 ];
 
 export const SUPPORTED_LANGS = allLangs.map(langData => ({
